54ffbc06a5d08323aa7dbd5207caf725b57e49ad
Ella Choy <egal21de@gmail.com>
2021-07-15 16:11:50 +0200

    finito

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,8 +11,14 @@ console.log(y)
 let z= x+y
 console.log(z)
 
-let firstName= "John"
-let lastName= "Doe"
-let age= 35
+// let firstName= "John"
+// let lastName= "Doe"
+// let age= 35
 
-console.log(firstName,lastName,35)
+// console.log(firstName,lastName,35)
+
+
+let firstName = "John", lastName = "Doe", age = 35;
+console.log(firstName);
+console.log(lastName);
+console.log(age);
