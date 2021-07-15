9ec5e0a809fa590fbc071b8b5bba4fe8feee9ba6
Ella Choy <egal21de@gmail.com>
2021-07-15 16:09:00 +0200

    var

---
+++ assets/js/main.js
@@ -0,0 +1,18 @@
+
+let name="BMW"
+console.log(name)
+
+let x=150
+console.log(x)
+
+let y=50
+console.log(y)
+
+let z= x+y
+console.log(z)
+
+let firstName= "John"
+let lastName= "Doe"
+let age= 35
+
+console.log(firstName,lastName,35)
